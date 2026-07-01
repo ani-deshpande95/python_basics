829d62e02f17fd22c68f1b0afc9b5998d05a680d
ani-deshpande95 <ani.deshpande95@gmail.com>
2026-07-01 15:01:53 +0000

    cybage training data

---
+++ cybage_training/python_day_3.py
@@ -0,0 +1,101 @@
+# name = input("enter your name : ")
+# age = int(input("Enter your age : "))
+
+# print(f"your name is : {name} and your age is : {age}")
+
+# if age >= 18 and age <= 99 :
+#     print('you are welcome to the club')
+# elif age >= 100:
+#     print("your are going to be in die club soon")
+# else:
+#     print("sorry!.... only adults are allowed into the club")
+#     print("Pleae come back after : {} years".format(18-age))
+
+# print(90 * '*')
+
+# answer = 5
+# print("Please enter number between 1 and 10 : ")
+# guess = int(input())
+
+# if guess != answer :
+#     if guess < answer :
+#        print("guess higher....")
+#     else :
+#         print("guess lower....")
+#     guess = int(input())
+#     if guess == answer :
+#         print("Correct......")
+#     else :
+#         print("sorry.... incorrect")
+# else :
+#     print("you are brilliant.....")
+
+
+# if guess == answer :
+#     print("You are brilliant....!")
+# else :
+#     if guess < answer :
+#        print("guess higher....!")
+#     else :
+#         print("guess lower....!")
+#     guess = int(input())
+#     if guess == answer :
+#         print("Correct......!")
+#     else :
+#         print("sorry.... incorrect guess!")
+
+
+
+# if guess < answer :
+#     print("guess higher")
+#     guess = int(input())
+#     if(guess == answer):
+#         print('Well done....!')
+#     else:
+#         print("Sorry....... wrong guess")    
+# elif guess > answer:
+#     print("guess lower")
+#     guess = int(input())
+#     if(guess == answer):
+#         print('Well done....!')
+#     else:
+#         print("Sorry....... wrong guess")
+# else:
+#     print("Correct.........!")
+
+# print(90 * '*')
+
+# age = int(input("Enter your age. :"))
+
+# if age >= 16 and age <= 65 :
+#     print("Welcome to Cybage....")
+ 
+# if 16 <= age <= 65 :                  # This is called as chained comparoson
+#     print("Welcome to Cybage....")
+# else : 
+#     print("Enjoy Mosco......!")
+
+# if age < 16 or age > 65 :           # This will do the same thing as previous condition
+#     print("Welcome to Cybage....")
+
+# print(90 * '*') 
+
+# day = "Sat"
+# temp = 30
+# raining = True
+
+# if day == "Sat" and temp > 27 and not raining:
+#     print("Swimming")
+# else:
+#     print("Do study")
+
+# if day == "Sat" and temp > 27 or not raining:
+#     print("Swimming")
+# else:
+#     print("Do study")
+
+
+
+print(90 * '*') 
+
+print(90 * '*') 
