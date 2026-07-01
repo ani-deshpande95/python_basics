02231956070ac34f72da0e5a496d8d63299007c5
ani-deshpande95 <ani.deshpande95@gmail.com>
2026-07-01 15:02:55 +0000

    cybage training data

---
+++ cybage_python_training/4_ranges.py
@@ -0,0 +1,21 @@
+for i in range(1, 20) :
+    print("i is now {}".format(i))
+
+print(90 * "*")
+
+print(90 * "*")
+
+
+print(90 * "*")
+
+
+print(90 * "*")
+
+
+print(90 * "*")
+
+
+print(90 * "*")
+
+
+print(90 * "*")
